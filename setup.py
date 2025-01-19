0d8e001e1511c4ddd3b4fd5614fe4f05be68dd99
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2025-01-19 19:42:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,15 +23,17 @@ with open(readme_path, "r", encoding="utf-8") as fh:
 
 setup(
     name='ReportMiner',
-    version='0.1.3',    
+    version='0.1.3',
     description='Easy to use wrapper for text document question and answer using RAG and LLM',
-    long_description=long_description,  
-    long_description_content_type="text/markdown",  
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/yohanesnuwara/reportminer',
     author='Yohanes Nuwara',
     author_email='ign.nuwara97@gmail.com',
     license='MIT License',
     packages=['reportminer'],
+
+    # Minimal install for basic functionality
     install_requires=[
         'byaldi>=0.0.7',
         'transformers>=4.46.3',
@@ -39,15 +41,35 @@ setup(
         'pillow>=11.1.0',
         'numpy>=1.26.4',
         'matplotlib>=3.10.0',
-        'pypdf2>=3.0.1',
-        'aspose-cells-python>=25.1.0',
-        'aspose.slides>=24.12.0',
-        'spire.presentation'>=9.12.1
+        'pypdf2>=3.0.1'
     ],
+
+    # Optional extras
+    extras_require={
+        # Install with `pip install reportminer[ppt]`
+        'ppt': [
+            'aspose.slides>=24.12.0',
+            'spire.presentation>=9.12.1'
+        ],
+
+        # Install with `pip install reportminer[xls]`
+        'xls': [
+            'aspose-cells-python>=25.1.0',
+        ],
+
+        # Install both PPT and XLS features with `pip install reportminer[all]`
+        # (optional "all" extra if you want everything at once)
+        'all': [
+            'aspose.slides>=24.12.0',
+            'spire.presentation>=9.12.1',
+            'aspose-cells-python>=25.1.0'
+        ],
+    },
+
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  
+        'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.10'
     ],
