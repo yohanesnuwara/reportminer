bb05900a8780cb9506c482be2d328b9cff525b10
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2025-02-18 19:17:59 +0100

    Create setup.sh
    shell file for setup in linux

---
+++ setup.sh
@@ -0,0 +1,9 @@
+apt-get install -y ttf-mscorefonts-installer 
+apt-get install fonts-dejavu
+apt-get install fonts-liberation
+
+apt-get update
+apt-get install -y poppler-utils
+
+apt install -y libgdiplus
+ln -s /usr/lib/libgdiplus.so /usr/lib/libgdiplus
