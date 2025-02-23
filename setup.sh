2d886d2275907d9fb8accbefae0e2fbcec56f6d6
yohanesnuwara <51282928+yohanesnuwara@users.noreply.github.com>
2025-02-23 19:26:54 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+# Run this shell file inside reportminer folder 
 apt-get install -y ttf-mscorefonts-installer 
 apt-get install fonts-dejavu
 apt-get install fonts-liberation
@@ -8,4 +9,8 @@ apt-get install -y poppler-utils
 apt install -y libgdiplus
 ln -s /usr/lib/libgdiplus.so /usr/lib/libgdiplus
 
+python3 -m venv .venv
+. .venv/bin/activate
+pip install Flask
+pip install matplotlib
 pip install .[xls,ppt]
